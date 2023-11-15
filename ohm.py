832e63ee6055cc1516ec175d738018dbf2498658
kobchaidontree23 <kobchaidontree23@gmail.com>
2023-11-16 01:20:56 +0700

    database

--- ohm.py
+++ ohm.py
@@ -1,1 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="cat"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
+
+
 
